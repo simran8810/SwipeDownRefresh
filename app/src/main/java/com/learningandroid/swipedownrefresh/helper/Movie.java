e67aef035242f71bbe9916bfdc945bc4099642e7
simran8810 <simran.kaur@hindustantimes.com>
2015-06-17 16:15:01 +0530

    swipedown refresh code

---
+++ app/src/main/java/com/learningandroid/swipedownrefresh/helper/Movie.java
@@ -0,0 +1,17 @@
+package com.learningandroid.swipedownrefresh.helper;
+
+/**
+ * Created by ht on 15/6/15.
+ */
+public class Movie {
+    public int id;
+    public String title;
+
+    public Movie() {
+    }
+
+    public Movie(int id, String title) {
+        this.title = title;
+        this.id = id;
+    }
+}
